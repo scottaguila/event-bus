349fb70f2664b712c2cf96f94a0f3ac8e5e44703
scottaguila <scott.aguila@gmail.com>
2024-03-14 20:08:20 -0400

    Add post to moderation service

--- index.js
+++ index.js
@@ -16,6 +16,9 @@ app.post("/events", (req, res) => {
   axios.post("http://localhost:4002/events", event).catch((e) => {
     console.log(e.message);
   });
+  axios.post("http://localhost:4003/events", event).catch((e) => {
+    console.log(e.message);
+  });
 
   res.send({ status: "OK" });
 });
