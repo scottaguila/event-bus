bb890df5d90f54fc2c143ce66e32f96866587df9
scottaguila <scott.aguila@gmail.com>
2024-03-17 08:06:56 -0400

    Create events storage in case query service goes offline

--- index.js
+++ index.js
@@ -1,28 +1,36 @@
-const express = require("express");
-const axios = require("axios");
+const express = require('express');
+const axios = require('axios');
 
 const app = express();
 app.use(express.json());
 
-app.post("/events", (req, res) => {
+const events = [];
+
+app.post('/events', (req, res) => {
   const event = req.body;
 
-  axios.post("http://localhost:4000/events", event).catch((e) => {
+  events.push(event);
+
+  axios.post('http://localhost:4000/events', event).catch((e) => {
     console.log(e.message);
   });
-  axios.post("http://localhost:4001/events", event).catch((e) => {
+  axios.post('http://localhost:4001/events', event).catch((e) => {
     console.log(e.message);
   });
-  axios.post("http://localhost:4002/events", event).catch((e) => {
+  axios.post('http://localhost:4002/events', event).catch((e) => {
     console.log(e.message);
   });
-  axios.post("http://localhost:4003/events", event).catch((e) => {
+  axios.post('http://localhost:4003/events', event).catch((e) => {
     console.log(e.message);
   });
 
-  res.send({ status: "OK" });
+  res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
-  console.log("Listening on 4005");
+  console.log('Listening on 4005');
 });
