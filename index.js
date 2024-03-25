3193cd993998ac47c48944b385e4c472cd5ee53e
scottaguila <scott.aguila@gmail.com>
2024-03-25 08:23:02 -0400

    Update axios post for posts cluster IP service

--- index.js
+++ index.js
@@ -11,18 +11,18 @@ app.post('/events', (req, res) => {
 
   events.push(event);
 
-  axios.post('http://localhost:4000/events', event).catch((e) => {
+  axios.post('http://posts-cluterip-srv:4000/events', event).catch((e) => {
     console.log(e.message);
   });
-  axios.post('http://localhost:4001/events', event).catch((e) => {
-    console.log(e.message);
-  });
-  axios.post('http://localhost:4002/events', event).catch((e) => {
-    console.log(e.message);
-  });
-  axios.post('http://localhost:4003/events', event).catch((e) => {
-    console.log(e.message);
-  });
+  // axios.post('http://localhost:4001/events', event).catch((e) => {
+  //   console.log(e.message);
+  // });
+  // axios.post('http://localhost:4002/events', event).catch((e) => {
+  //   console.log(e.message);
+  // });
+  // axios.post('http://localhost:4003/events', event).catch((e) => {
+  //   console.log(e.message);
+  // });
 
   res.send({ status: 'OK' });
 });
